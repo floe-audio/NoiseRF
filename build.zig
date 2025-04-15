87510198a03cb4d7263006a5b6ab203edd1f47fa
Sam Windell <info@frozenplain.com>
2025-04-15 09:12:17 +0100

    CI: clap-validator for all os using Zig rather than ad-hoc scripts

--- build.zig
+++ build.zig
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: LGPL-3.0
 
 const std = @import("std");
+const builtin = @import("builtin");
 
 const project_name = "NoiseRF";
 const project_version = "0.1.0";
@@ -95,19 +96,110 @@ pub fn build(b: *std.Build) void {
         .flags = flags,
     });
 
-    switch (target.result.os.tag) {
-        .linux => {
-            LinuxPluginInstallStep.add(b, plugin) catch unreachable;
-        },
-        .windows => {
-            WindowsPluginStep.add(b, plugin) catch unreachable;
-        },
-        .macos => {
-            MacosPluginInstallStep.add(b, plugin) catch unreachable;
-        },
-        else => {},
+    const test_step = b.step("test", "");
+
+    const install = PluginInstallStep.add(b, plugin) catch unreachable;
+    addClapValidatorIfNeeded(b, test_step, install) catch unreachable;
+}
+
+fn addClapValidatorIfNeeded(b: *std.Build, test_step: *std.Build.Step, install_step: *PluginInstallStep) !void {
+    var clap_validator: ?std.Build.LazyPath = null;
+
+    if (builtin.os.tag == install_step.lib_step.rootModuleTarget().os.tag) {
+        switch (builtin.os.tag) {
+            .macos => {
+                clap_validator = b.dependency("clap_validator_macos", .{}).path("clap-validator");
+            },
+            .windows => {
+                clap_validator = b.dependency("clap_validator_windows", .{}).path("clap-validator.exe");
+            },
+            .linux => {
+                if (try isUbuntu()) {
+                    clap_validator = b.dependency("clap_validator_ubuntu", .{}).path("clap-validator");
+                }
+            },
+            else => {
+                // Unsupported OS for clap-validator
+            },
+        }
+    }
+
+    if (clap_validator) |c| {
+        ClapValidatorStep.add(b, test_step, install_step, c) catch unreachable;
     }
 }
+
+fn isUbuntu() !bool {
+    const file = try std.fs.cwd().openFile("/etc/os-release", .{});
+    defer file.close();
+
+    var buffer: [1024]u8 = undefined;
+    const reader = file.reader();
+
+    while (try reader.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
+        if (std.mem.indexOf(u8, line, "ID=ubuntu") != null) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+const ClapValidatorStep = struct {
+    step: std.Build.Step,
+    plugin_step: *PluginInstallStep,
+    clap_validator: std.Build.LazyPath,
+
+    pub fn add(
+        b: *std.Build,
+        test_step: *std.Build.Step,
+        plugin_step: *PluginInstallStep,
+        clap_validator: std.Build.LazyPath,
+    ) !void {
+        const validate = b.allocator.create(ClapValidatorStep) catch unreachable;
+        validate.step = std.Build.Step.init(.{
+            .id = std.Build.Step.Id.custom,
+            .name = "clap-validator",
+            .owner = b,
+            .makeFn = make,
+        });
+        validate.plugin_step = plugin_step;
+        validate.clap_validator = clap_validator;
+        validate.step.dependOn(&plugin_step.step);
+        test_step.dependOn(&validate.step);
+    }
+
+    fn make(step: *std.Build.Step, prog_node: std.Progress.Node) !void {
+        _ = prog_node;
+        const self: *ClapValidatorStep = @fieldParentPtr("step", step);
+        const b = step.owner;
+
+        const clap_path = self.plugin_step.clap_path.?;
+        const clap_validator = self.clap_validator.getPath2(b, step);
+
+        {
+            const clap_validator_file = try std.fs.openFileAbsolute(clap_validator, .{});
+            defer clap_validator_file.close();
+            try clap_validator_file.chmod(0o755);
+        }
+
+        const args = [_][]const u8{
+            clap_validator,
+            "validate",
+            "--test-filter",
+            ".*state.*",
+            "--invert-filter",
+            clap_path,
+        };
+
+        var out_code: u8 = 0;
+        const out = try b.runAllowFail(&args, &out_code, .Inherit);
+
+        if (out_code != 0) {
+            return step.fail("clap-validator failed with code {d}\n{s}", .{ out_code, out });
+        }
+    }
+};
 
 fn installPath(b: *std.Build, step: *std.Build.Step.Compile) []const u8 {
     // It's called linuxTriple but it can be used for any OS.
@@ -115,19 +207,20 @@ fn installPath(b: *std.Build, step: *std.Build.Step.Compile) []const u8 {
     return b.getInstallPath(.prefix, folder);
 }
 
-const MacosPluginInstallStep = struct {
+const PluginInstallStep = struct {
     step: std.Build.Step,
     lib_step: *std.Build.Step.Compile,
     emitted_bin: ?std.Build.LazyPath,
+    clap_path: ?[]const u8,
 
     pub fn add(
         b: *std.Build,
         plugin: *std.Build.Step.Compile,
-    ) !void {
-        const install = b.allocator.create(MacosPluginInstallStep) catch unreachable;
+    ) !*PluginInstallStep {
+        const install = b.allocator.create(PluginInstallStep) catch unreachable;
         install.step = std.Build.Step.init(.{
             .id = std.Build.Step.Id.custom,
-            .name = "macos-make-bundle",
+            .name = "plugin-install",
             .owner = b,
             .makeFn = make,
         });
@@ -135,45 +228,59 @@ const MacosPluginInstallStep = struct {
         install.emitted_bin = plugin.getEmittedBin();
         install.step.dependOn(&plugin.step);
         b.default_step.dependOn(&install.step);
+        return install;
     }
 
     fn make(step: *std.Build.Step, prog_node: std.Progress.Node) !void {
         _ = prog_node;
-        const self: *MacosPluginInstallStep = @fieldParentPtr("step", step);
+        const self: *PluginInstallStep = @fieldParentPtr("step", step);
         const b = step.owner;
 
         const folder = installPath(b, self.lib_step);
-
-        const bundle_name = b.fmt("{s}.clap", .{self.lib_step.name});
-        const bundle_path = try std.fs.path.join(b.allocator, &[_][]const u8{
+        const filename = b.fmt("{s}.clap", .{self.lib_step.name});
+        const clap_path = try std.fs.path.join(b.allocator, &[_][]const u8{
             folder,
-            bundle_name,
+            filename,
         });
-
-        const bundle_exec_path = try std.fs.path.join(b.allocator, &[_][]const u8{
-            bundle_path,
-            "Contents",
-            "MacOS",
-            self.lib_step.name,
-        });
+        self.clap_path = clap_path;
 
         const source = self.emitted_bin.?.getPath2(step.owner, step);
 
-        _ = try std.fs.updateFileAbsolute(source, bundle_exec_path, .{});
+        switch (self.lib_step.rootModuleTarget().os.tag) {
+            .macos => {
+                // On macOS we need to create a bundle.
+                const bundle_exec_path = try std.fs.path.join(b.allocator, &[_][]const u8{
+                    clap_path,
+                    "Contents",
+                    "MacOS",
+                    self.lib_step.name,
+                });
 
-        var bundle_dir = try std.fs.openDirAbsolute(bundle_path, .{});
-        defer bundle_dir.close();
+                _ = try std.fs.updateFileAbsolute(source, bundle_exec_path, .{});
 
-        const plist_file = try bundle_dir.createFile("Contents/Info.plist", .{});
-        defer plist_file.close();
-        try self.writePlist(plist_file);
+                var bundle_dir = try std.fs.openDirAbsolute(clap_path, .{});
+                defer bundle_dir.close();
 
-        const pkginfo_file = try bundle_dir.createFile("Contents/PkgInfo", .{});
-        defer pkginfo_file.close();
-        try pkginfo_file.writeAll("BNDL????");
+                const plist_file = try bundle_dir.createFile("Contents/Info.plist", .{});
+                defer plist_file.close();
+                try self.writePlist(plist_file);
+
+                const pkginfo_file = try bundle_dir.createFile("Contents/PkgInfo", .{});
+                defer pkginfo_file.close();
+                try pkginfo_file.writeAll("BNDL????");
+            },
+            .windows, .linux => {
+                // For Windows and Linux, we just copy the file but use the clap extension rather than
+                // whatever the default would be (dll/lib*.so)
+                _ = try std.fs.updateFileAbsolute(source, clap_path, .{});
+            },
+            else => unreachable,
+        }
     }
 
-    fn writePlist(self: *MacosPluginInstallStep, file: std.fs.File) !void {
+    fn writePlist(self: *PluginInstallStep, file: std.fs.File) !void {
+        std.debug.assert(self.lib_step.rootModuleTarget().os.tag == .macos);
+
         const writer = file.writer();
         const template = @embedFile("resources/Info.plist");
 
@@ -191,86 +298,5 @@ const MacosPluginInstallStep = struct {
                 .copyright = "Copyright (c) 2022 Luciano Dato and 2025 Floe Audio",
             },
         ) catch unreachable;
-    }
-};
-
-const LinuxPluginInstallStep = struct {
-    step: std.Build.Step,
-    lib_step: *std.Build.Step.Compile,
-    emitted_bin: ?std.Build.LazyPath,
-
-    pub fn add(
-        b: *std.Build,
-        plugin: *std.Build.Step.Compile,
-    ) !void {
-        const install = b.allocator.create(LinuxPluginInstallStep) catch unreachable;
-        install.step = std.Build.Step.init(.{
-            .id = std.Build.Step.Id.custom,
-            .name = "linux-finalise",
-            .owner = b,
-            .makeFn = make,
-        });
-        install.lib_step = plugin;
-        install.emitted_bin = plugin.getEmittedBin();
-        install.step.dependOn(&plugin.step);
-        b.default_step.dependOn(&install.step);
-    }
-
-    fn make(step: *std.Build.Step, prog_node: std.Progress.Node) !void {
-        _ = prog_node;
-        const self: *LinuxPluginInstallStep = @fieldParentPtr("step", step);
-        const b = step.owner;
-
-        const folder = installPath(b, self.lib_step);
-        const filename = b.fmt("{s}.clap", .{self.lib_step.name});
-        const clap_path = try std.fs.path.join(b.allocator, &[_][]const u8{
-            folder,
-            filename,
-        });
-
-        const source = self.emitted_bin.?.getPath2(step.owner, step);
-
-        _ = try std.fs.updateFileAbsolute(source, clap_path, .{});
-    }
-};
-
-// For Windows, we need to do the same as Linux, except we are simply replacing the dll extension with a clap extension.
-const WindowsPluginStep = struct {
-    step: std.Build.Step,
-    lib_step: *std.Build.Step.Compile,
-    emitted_bin: ?std.Build.LazyPath,
-
-    pub fn add(
-        b: *std.Build,
-        plugin: *std.Build.Step.Compile,
-    ) !void {
-        const install = b.allocator.create(WindowsPluginStep) catch unreachable;
-        install.step = std.Build.Step.init(.{
-            .id = std.Build.Step.Id.custom,
-            .name = "windows-finalise",
-            .owner = b,
-            .makeFn = make,
-        });
-        install.lib_step = plugin;
-        install.emitted_bin = plugin.getEmittedBin();
-        install.step.dependOn(&plugin.step);
-        b.default_step.dependOn(&install.step);
-    }
-
-    fn make(step: *std.Build.Step, prog_node: std.Progress.Node) !void {
-        _ = prog_node;
-        const self: *WindowsPluginStep = @fieldParentPtr("step", step);
-        const b = step.owner;
-
-        const folder = installPath(b, self.lib_step);
-        const filename = b.fmt("{s}.clap", .{self.lib_step.name});
-        const clap_path = try std.fs.path.join(b.allocator, &[_][]const u8{
-            folder,
-            filename,
-        });
-
-        const source = self.emitted_bin.?.getPath2(step.owner, step);
-
-        _ = try std.fs.updateFileAbsolute(source, clap_path, .{});
     }
 };
